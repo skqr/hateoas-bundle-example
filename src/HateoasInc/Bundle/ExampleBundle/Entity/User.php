08bd9c33e22b591095fc795a9134f293038f36cf
Javier Lorenzana <skqr.io@gmail.com>
2014-12-16 10:57:23 -0300

    https://github.com/skqr/hateoas-bundle-example/issues/8

--- src/HateoasInc/Bundle/ExampleBundle/Entity/User.php
+++ src/HateoasInc/Bundle/ExampleBundle/Entity/User.php
@@ -92,6 +92,7 @@ class User implements UserInterface, ResourceEntityInterface
     public function __construct()
     {
         $this->followers = new ArrayCollection();
+        $this->userGroups = new ArrayCollection();
         $this->salt = md5(uniqid(NULL, TRUE));
     }
 
